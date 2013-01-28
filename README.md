03241b883c9dda16607618c5eba5b6d49e52b7e5
rblalock <rickblalock@mac.com>
2013-01-28 11:10:07 -0500

    v1.1

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-GithubWebHookForTeamworkPM
+A Github Webhook for TeamworkPM
 ==========================
 
-A Github web hook for teamworkpm.net
+First you need to change two variables: 
+1. Change user token to the user's token that will post on Github's behalf. 
+2. Change the URL to your TeamworkPM's url (including the resource you'd like the commits to post to, e.g. /task/) and you're ready to go.
+
+When committing to Github you can reference a TeamworkPM task via it's ID with `#`.  E.g. "Fixes #123456" where #123456 is the TeamworkPM Task ID (or as the API calls it, the Teamwork Resource ID - https://yoursite.teamworkpm.net/tasks/123456).
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * GitHub Post to TeamworkPM
+ * @version 1.1
+ */
+
+// CHANGE ME - Root URL for the Teamwork API call
+$URL = ''; // e.g. https://yoursite.teamworkpm.net/tasks/
+
+// TeamworkPM user token used to post a comment on behalf of the Github commit
+$USER_TOKEN = '';
+
+try {
+    // Convert the post data
+    $data = stripslashes( $_POST['payload'] );
+    $postdata = json_decode($data);
+
+    if($_POST['payload'] && $postdata) {
+        // Iterate through each commit to see if we have a related task
+        foreach ($postdata->commits as $commit) {
+            // Format message data
+            $commidID   = $commit->id;
+            $comment    = $commit->message;
+            $url        = $commit->url;
+            $timestamp  = $commit->timestamp;
+            $author     = $commit->author->name;
+
+            // Get any commit messages that have a # tag (points ot a resource ID in Teamwork)
+            preg_match_all('/#([A-Za-z0-9_]+)/', $comment, $matches);
+            // Remove the first index since it's the original
+            $resourceID = array_pop($matches);
+
+            // Format the message that will post to Teamwork
+            $body = $comment . "\n\n" .
+                    $url . "\n\n" .
+                    "committed by " . $author . " on " . date('m/d/Y', strtotime($timestamp));
+            $params = array(
+                'comment' => array(
+                    'body' => $body
+                )
+            );
+            $postData = json_encode($params);
+
+            if(count($resourceID) > 0) {
+                // Iterate through each hash tag / resource and make a request
+                foreach ($resourceID as $resource) {
+                    // Create the comment
+                    $c = curl_init();
+                    $headers = array(
+                        'Authorization: BASIC '. base64_encode($USER_TOKEN . ':xxxzzz'),
+                        'Content-Type: application/json',
+                        'Accept: application/json'
+                    );
+                    curl_setopt_array($c, array(
+                        CURLOPT_RETURNTRANSFER => TRUE,
+                        CURLOPT_HEADER         => TRUE,
+                        CURLOPT_SSL_VERIFYHOST => FALSE,
+                        CURLOPT_SSL_VERIFYPEER => FALSE,
+                        CURLOPT_POST => TRUE,
+                        CURLOPT_HTTPHEADER => $headers
+                    ));
+                    curl_setopt($c, CURLOPT_URL, $URL . $resource . '/comments.json');
+                    curl_setopt($c, CURLOPT_POSTFIELDS, $postData );
+
+                    $response = curl_exec($c);
+                    $httpCode = curl_getinfo($c, CURLINFO_HTTP_CODE);
+                    curl_close($c);
+                }
+            }
+        }
+    }
+
+} catch (Exception $e) {
+    print_r($e);
+}
